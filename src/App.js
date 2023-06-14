8ec02abce5ce1677a7bd0bad16ef3c02b6edc756
fa9876 <ipfa.eva@gmail.com>
2023-06-14 20:01:16 +0800

    first answer commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import ListItem from "./ListItem";
 
 class App extends React.Component {
   state = {
-    selectData: "",
+    selectedItems: [],
   };
 
-  handleDateClick = (newSelectDate) => {
-    const newdata = this.state.selectData;
-    this.setState({ selectData: newdata + newSelectDate });
+  listItemChangeHandler = (item) => {
+    //
+    let newSelectedItems = [...this.state.selectedItems];
+
+    if (newSelectedItems.includes(item)) {
+      newSelectedItems = this.state.selectedItems.filter(
+        (theItem) => item !== theItem
+      );
+    } else {
+      newSelectedItems.push(item);
+    }
+
+    this.setState({
+      selectedItems: newSelectedItems,
+    });
   };
 
   render() {
@@ -17,48 +30,34 @@ class App extends React.Component {
           <div className="w-1/2">
             <h2 className="text-2xl font-bold">Apple Products</h2>
             <ul className="mt-4 flex flex-col gap-y-3">
-              <Block onDataUpdate={this.handleDateClick} title="iPhone" />
-              <Block onDataUpdate={this.handleDateClick} title="ipad" />
-              <Block onDataUpdate={this.handleDateClick} title="Mac" />
-              <li className="bg-white px-6 py-4 rounded-lg shadow hover:cursor-pointer bg-slate-800 transition">
-                Mac
-              </li>
-              <li className="bg-white px-6 py-4 rounded-lg shadow hover:cursor-pointer hover:bg-slate-100 transition">
-                Apple TV
-              </li>
-              <li className="bg-white px-6 py-4 rounded-lg shadow hover:cursor-pointer hover:bg-slate-100 transition">
-                HomePod
-              </li>
+              {["iPhone", "iPad", "MacBook", "Apple TV", "HomePod"].map(
+                (item) => {
+                  return (
+                    <ListItem
+                      key={item}
+                      active={this.state.selectedItems.includes(item)}
+                      onChange={this.listItemChangeHandler}
+                    >
+                      {item}
+                    </ListItem>
+                  );
+                }
+              )}
             </ul>
-            <p className="mt-3 text-slate-400 text-sm">2 item(s) selected</p>
+            <p className="mt-3 text-slate-400 text-sm">
+              {this.state.selectedItems.length} item(s) selected
+            </p>
           </div>
           <div className="w-1/2">
             <h2 className="text-2xl font-bold text-slate-400">
               Selected Products
             </h2>
             <p className="mt-4 text-slate-800 text-lg">
-              {this.state.selectData}
+              {this.state.selectedItems.join(", ")}
             </p>
           </div>
         </div>
       </div>
-    );
-  }
-}
-
-class Block extends React.Component {
-  handleClick = () => {
-    const newData = this.props.title;
-    this.props.onDataUpdate(newData);
-  };
-  render() {
-    return (
-      <li
-        onClick={this.handleDateClick}
-        className="bg-white px-6 py-4 rounded-lg shadow hover:cursor-pointer bg-slate-800 transition"
-      >
-        {this.props.title}
-      </li>
     );
   }
 }
